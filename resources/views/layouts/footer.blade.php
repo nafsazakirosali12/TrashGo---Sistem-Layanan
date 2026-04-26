5222d9ad5ef53cfeca8301d972ee4082565b6d8c
nndlizz <nndlizz.06@gmail.com>
2026-04-26 12:19:52 +0700

    merapikan navbar dan footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
- <footer class="footer pt-3  ">
+ <!-- <footer class="footer pt-3  ">
         <div class="container-fluid">
           <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-              <div class="copyright text-center text-sm text-muted text-lg-start">
+            <div class="col-lg-6 mb-lg-0 mb-4 text-center">
+              <div class="copyright text-center text-sm text-muted">
                 © <script>
                   document.write(new Date().getFullYear())
                 </script>,
@@ -11,26 +11,21 @@
                 for a better environment.
               </div>
             </div>
-            <div class="col-lg-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-                </li>
-              </ul>
+            
+          </div>
+        </div>
+      </footer> -->
+      <footer class="footer pt-3 mt-auto">
+        <div class="container-fluid fixed-bottom" style="bottom:10 px">
+          <div class="row">
+            <div class="col-12 text-center">
+              <div class="text-center text-sm text-muted">
+                © {{ date('Y') }},
+                made with <i class="fa fa-heart"></i> by
+                <span class="fw-bold">RecyCode Team</span>
+                for a better environment.
+              </div>
             </div>
           </div>
         </div>
       </footer>
-
-  
-
-    
