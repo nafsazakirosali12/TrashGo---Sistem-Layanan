a541d01eea4d5da25d08dbf6e80ecf0ea1a6d450
Citrasbna <citrasyabaniagustin@gmail.com>
2026-05-03 13:29:41 +0700

    perubahan

--- resources/views/admin/monitoring.blade.php
+++ resources/views/admin/monitoring.blade.php
@@ -22,14 +22,14 @@
                     <!-- THEAD -->
                      <thead>
                         <tr  style="text-align: center;">
-                            <th>No</th>
-                            <th>ID</th>
-                            <th>Masyarakat</th>
-                            <th>Kategori</th>
-                            <th>Status</th>
-                            <th>Total Harga</th>
-                            <th>Tanggal</th>
-                            <th>Catatan</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">No</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">ID</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Masyarakat</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Kategori</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Status</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Total Harga</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Tanggal</th>
+                            <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">Catatan</th>
                         </tr>
                      </thead>
 
