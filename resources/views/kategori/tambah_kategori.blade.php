3d360038dc61d7b51cd83eb7c4ac19aff3f57e34
Citrasbna <citrasyabaniagustin@gmail.com>
2026-05-06 16:10:34 +0700

    memperbaiki tambah, dan edit kategori, menambahkan faq dan galeri

--- resources/views/kategori/tambah_kategori.blade.php
+++ resources/views/kategori/tambah_kategori.blade.php
@@ -52,7 +52,7 @@
                 name="deskripsi" 
                 class="form-control"
                 rows="3"
-                placeholder="Masukkan deskripsi (opsional)"
+                placeholder="Masukkan deskripsi"
               >{{ old('deskripsi') }}</textarea>
             </div>
 
