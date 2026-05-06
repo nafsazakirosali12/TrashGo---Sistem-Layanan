153e543dd328f82c38414a9870f50083d001ecd3
nndlizz <nndlizz.06@gmail.com>
2026-05-06 12:30:59 +0700

    part kesekian

--- resources/views/admin/tambah-akun/index.blade.php
+++ resources/views/admin/tambah-akun/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('page', 'Akun Petugas')
+
 @section('content')
 
 <div class="container-fluid py-4">
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('page', 'Dashboard')
+
 @section('content')
 
 <div class="container-fluid py-4">
